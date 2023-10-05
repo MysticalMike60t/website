63812e66dde84a3dbf65ebef3fd5fe649d39ea3e
Mysticmike60t <caden.finkelstein@cttech.org>
2023-10-04 22:42:10 -0400

    sd

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,18 @@
 import React from "react";
-import { FullScreenLoader } from "./components/global/modules";
+import { FullScreenLoader, Layout } from "./components/global/modules";
+import { NoPage } from "./components/global/pages";
+import { Route, Routes } from "react-router-dom";
+import { Home } from "./components/pages";
 
 function App() {
   return (
     <React.Suspense fallback={<FullScreenLoader />}>
-
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="*" element={<NoPage />} />
+        </Route>
+      </Routes>
     </React.Suspense>
   );
 }
